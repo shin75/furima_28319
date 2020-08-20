4ae58e015e97c43ad4393a464b01b556d96d4628
shin75 <68498224+shin75@users.noreply.github.com>
2020-08-20 16:44:26 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
         
- validates :name, presence: true
+  with_options presence:true do
+    validates :name
+    validates :birth_day
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/} 
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/} 
+  end
 
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
 end
